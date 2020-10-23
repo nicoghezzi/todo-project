6bb0758c49963b92451c8ec255fcb478fedc4992
Nico Ghezzi <nicolas.dario.ghezzi@gmail.com>
2020-10-22 20:09:51 -0400

    Added List of Todos

--- todo/forms.py
+++ todo/forms.py
@@ -1,3 +1,7 @@
+from django.forms import ModelForm
+from .models import Todo
 
-
-class TodoForm
+class TodoForm(ModelForm):
+    class Meta:
+        model = Todo
+        fields = ['title', 'memo', 'important']
--- todo/views.py
+++ todo/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
+from .forms import TodoForm
+from .models import Todo
 
 def home(request):
     return render(request, 'todo/home.html')
@@ -41,9 +43,17 @@ def logoutuser(request):
 
 def createtodo(request):
     if request.method == 'GET':
-        return render(request, 'todo/createtodo.html', {'form':AuthenticationForm()})
+        return render(request, 'todo/createtodo.html', {'form':TodoForm()})
     else:
-        pass
+        try:
+            form = TodoForm(request.POST)
+            newtodo = form.save(commit=False)
+            newtodo.user = request.user
+            newtodo.save()
+            return redirect('currenttodos')
+        except ValueError:
+            return render(request, 'todo/createtodo.html', {'form':TodoForm(), 'error': 'Bad data passed in. Try again'})
 
 def currenttodos(request):
-    return render(request, 'todo/currenttodos.html')
+    todos = Todo.objects.filter(user=request.user, datecompleted__isnull=True)
+    return render(request, 'todo/currenttodos.html', {'todos': todos})
