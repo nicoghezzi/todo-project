a6e706b724322e95616328ebe11b1ecf8b8c6260
Nico Ghezzi <nicolas.dario.ghezzi@gmail.com>
2020-10-24 13:42:04 -0400

    Added viewtodos

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
@@ -57,3 +57,9 @@ def createtodo(request):
 def currenttodos(request):
     todos = Todo.objects.filter(user=request.user, datecompleted__isnull=True)
     return render(request, 'todo/currenttodos.html', {'todos': todos})
+
+
+def viewtodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk)
+    form = TodoForm(instance=todo)
+    return render(request, 'todo/viewtodo.html', {'todo': todo, 'form':form})
